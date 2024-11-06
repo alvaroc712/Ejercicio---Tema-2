ec6a2156541343ed5d8f5fedb3c3a6ef147d313c
alvaroc712 <alvarocozano@gmail.com>
2024-11-06 18:38:19 +0100

    Base

---
+++ web/public/javascript/script.js
@@ -0,0 +1,4 @@
+document.getElementById("toggle-login").addEventListener("click", function() {
+    const loginBar = document.getElementById("login-bar");
+    loginBar.classList.toggle("hidden");
+});
